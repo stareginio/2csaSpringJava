c5fd8803a81f24c7506e910cb15edb49cf627101
stareginio <sophiecodes02@gmail.com>
2021-12-11 18:32:36 +0800

    Update EmployeeController.java
    renamed employee_id into employeeId, replaced int with Long for the employeeId

--- src/main/java/com/csa/samplefullstack/controllers/EmployeeController.java
+++ src/main/java/com/csa/samplefullstack/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import com.csa.samplefullstack.entities.Employee;
 import com.csa.samplefullstack.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
 
 import java.util.*;
 
@@ -15,8 +16,29 @@ public class EmployeeController {
     @Autowired  // spring - dependency injection
     private EmployeeRepository employeeRepository;
     
-    @RequestMapping("/employees")
+    @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+    
+    @PostMapping("/employees")
+    public Employee postEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+    
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeFromId(@PathVariable Long id){
+        return employeeRepository.findById(id).get();
+    }
+    
+    @PutMapping("/employees")
+    public Employee getEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+    
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable Long id){
+        employeeRepository.deleteById(id);
+        return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
     }
 }
