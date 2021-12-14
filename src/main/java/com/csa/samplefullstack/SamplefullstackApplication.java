4d98896ed2f77fee653eae8506472a1245060402
stareginio <sophiecodes02@gmail.com>
2021-12-15 05:09:36 +0800

    Update SamplefullstackApplication.java

--- src/main/java/com/csa/samplefullstack/SamplefullstackApplication.java
+++ src/main/java/com/csa/samplefullstack/SamplefullstackApplication.java
@@ -2,10 +2,8 @@ package com.csa.samplefullstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class SamplefullstackApplication {
 
 	public static void main(String[] args) {
