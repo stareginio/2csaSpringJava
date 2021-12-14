5ad76d006065664300c7d5d366d1526be1368f0d
stareginio <sophiecodes02@gmail.com>
2021-12-15 04:28:49 +0800

    Create ErrorController.java

---
+++ src/main/java/com/csa/samplefullstack/controllers/ErrorController.java
@@ -0,0 +1,18 @@
+@RestController
+public class IndexController implements ErrorController{
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
+
+// reference
+// https://stackoverflow.com/questions/25356781/spring-boot-remove-whitelabel-error-page
